fa3bd8d5a980cd620d627f5e6a4b9efa7aae1d28
namn310 <namn3102003@gmail.com>
2024-08-30 16:00:33 +0700

    update db

--- Connection/Connection.php
+++ Connection/Connection.php
@@ -3,7 +3,7 @@ class Connection
 {
     public static function GetConnect()
     {
-        $conn = new PDO("mysql:hostname=localhost;dbname=petcaredb", "root", "");
+        $conn = new PDO("mysql:hostname=localhost;dbname=examproject", "root", "");
         $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
         $conn->exec("set names utf8");
         return $conn;
