b8be06a07742df1fe0b8fa2fe46afea563697e63
LaDucHiep2003 <duchiep045@gmail.com>
2024-08-31 09:39:41 +0700

    update

--- Models/QuestionsModal.php
+++ Models/QuestionsModal.php
@@ -11,9 +11,18 @@ class QuestionModal
     // Lay tat ca data bang question
     public function read()
     {
-        $query = $this->conn->prepare("SELECT * FROM questions");
+        $query = $this->conn->prepare("SELECT * FROM questions where deleted = false");
         $query->execute();
         return $query->fetchAll();
+    }
+
+    public function delete($id)
+    {
+        $query = $this->conn->prepare("update questions set deleted = true where id=:id");
+        if($query->execute(['id' => $id])){
+            return true;
+        }
+        return false;
     }
 }
 ?>
