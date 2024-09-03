6722eb5ab9198cdccb5a3c4c5e3e2162ce93b54c
LaDucHiep2003 <duchiep045@gmail.com>
2024-09-03 17:39:52 +0700

    delete exams

--- Routes/router.php
+++ Routes/router.php
@@ -1,7 +1,7 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
-header('Access-Control-Allow-Methods: PUT');
+header('Access-Control-Allow-Methods: PUT, DELETE');
 header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
 include_once  __DIR__ . '/../Controllers/QuestionController.php';
